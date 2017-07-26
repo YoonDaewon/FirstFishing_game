fa4c146e9ce9dae13233783bfb41085b97b5ff48
YoonDaewon <ydwinha@naver.com>
2017-07-26 14:23:59 +0900

    config, lib 추가

---
+++ config/developement.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    mysql: {
+        master: {
+            connectionLimit: 100,
+            host: "13.124.158.58",
+            user: "root",
+            password: "qwe123"
+        }
+    },
+    // HTTP Communication Enc/Dec
+    encryption: {
+        state: true,
+        key : "C0LTUK3mgDyjzAB09Ui8BIwTCkkR2sbJ06OnKvJq84M=",
+        iv :  "hJrN5XisGO652h2pFNSZ6A=="
+    }    
+};
